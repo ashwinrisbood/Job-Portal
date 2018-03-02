291badfe882ce9124391997670c2c9233217379d
Ashwin <arisboo@ncsu.edu>
2018-03-02 01:12:04 -0500

    filter functionality added

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,11 @@
 class Job < ApplicationRecord
   belongs_to :company
+  def self.search(params)
+    results = Job.all
+    results = results.where(company_id: params[:company_id]) if params[:company_id].present?
+    if params[:search].present?
+      results = results.joins(:company).where('companies.name LIKE :search OR companies.industry LIKE :search OR companies.Hq LIke :search',search: "%#{params[:search]}%")
+    end
+    results
+  end
 end
