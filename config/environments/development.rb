48441479971ba123eca80ffc3f322b4ec9dfe596
Sameer Poudwal <spoudwa@ncsu.edu>
2018-03-05 12:06:13 -0500

    errors removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,9 +42,8 @@ Rails.application.configure do
       port: 587,
       authentication: "plain",
       enable_starttls_auto: true,
-      user_name: "pranssin@gmail.com",
-      password: "codwarfare"
-
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
   }
 
 
